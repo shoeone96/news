53a2b0b26b52bde82473444aedb428505efdecf2
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2025-02-26 23:30:07 +0900

    feat: 주소 변경

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY = `3ab184f8ae2e43c3a3c4f717a579bb1a`;
 let news = [];
 const getLatestNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&apiKey=${API_KEY}`);
     const response = await (await fetch(url)).json();
     news = response.articles;
     console.log(response);
